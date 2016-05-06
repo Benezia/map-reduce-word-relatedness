8108802ca24316b11d8e3ae269809a6553816c0a
benezia <benezia@ubuntu>
2016-05-06 15:28:37 +0300

    Changed 'Pairs' to 'Stripes' representation, Added Logger output.

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,15 +1,20 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -19,14 +24,16 @@ import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 
 public class WordCount {
 	final static String STOP_WORDS_FILE = "wc.stopwords.file";
-	
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	private static FileHandler logFile;
+	private static Logger logger;
+
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, MapWritable> {
 		Set<String> stopWords = new HashSet<String>();
-		//private final static IntWritable one = new IntWritable(1);
+		private MapWritable occurrenceMap = new MapWritable();
 		private Text word = new Text();
 		
         @Override
-        protected void setup(Context context) throws IOException{
+        protected void setup(Context context) throws IOException {
             Configuration conf = context.getConfiguration();
             String stp_file_name = conf.get(STOP_WORDS_FILE);
             try {
@@ -36,7 +43,6 @@ public class WordCount {
 
                 String word;
                 while((word = fis.readLine()) != null) {
-                	//System.out.println("GOT: "+ word);
                     stopWords.add(word);
                 }
                 fis.close();
@@ -47,45 +53,84 @@ public class WordCount {
         }
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			//System.out.println("GOT: "+ value.toString());
 			String[] dataRow = value.toString().split("\t");
-			// split positions:
-			// n-gram - The actual n-gram
-			// year - The year for this aggregation
-			// occurrences - The number of times this n-gram appeared in this year
-			// pages - The number of pages this n-gram appeared on in this year
-			// books - The number of books this n-gram appeared in during this year
+
+			/*
+			 split positions:
+			 n-gram			(dataRow[0]) - The actual n-gram
+			 year			(dataRow[1]) - The year for this aggregation
+			 occurrences 	(dataRow[2]) - The number of times this n-gram appeared in this year
+			 pages  		(dataRow[3]) - The number of pages this n-gram appeared on in this year
+			 books 			(dataRow[4]) - The number of books this n-gram appeared in during this year
+			*/
 			IntWritable occurrences = new IntWritable(Integer.parseInt(dataRow[2]));
 			int year = Integer.parseInt(dataRow[1]);
 			String[] ngrams = dataRow[0].split("\\s+");
 			
-			
-			if (ngrams.length != 5)
+			if (ngrams.length != 5 || year < 1900) 
 				return;
 			
+			logger.log(Level.FINE, value.toString());
+
 			String mid = ngrams[2].toLowerCase();
 
 			if(stopWords.contains(mid))
 				return;
-			
-			for (int i : new int[]{0,1,3,4}) {
-				String curr = ngrams[i].toLowerCase();
-				
-				if (!stopWords.contains(curr)) {
-					word.set(mid + "," + curr);
-					context.write(word, occurrences);
-				}
-			}
-			
-		}
-	}
 
+	          word.set(mid);
+	          occurrenceMap.clear();
+
+			for (int j : new int[]{0,1,3,4}) {
+				String curr = ngrams[j].toLowerCase();
+                Text neighbor = new Text(curr);
+                if (!stopWords.contains(curr)) {
+	                if(occurrenceMap.containsKey(neighbor)) {
+	                   IntWritable count = (IntWritable)occurrenceMap.get(neighbor);
+	                   count.set(count.get()+occurrences.get());
+	                } else 
+	                   occurrenceMap.put(neighbor,occurrences);
+                }
+	        }
+			context.write(word,occurrenceMap);
+	     }
+   }
+
+
+	
+
+public static class StripesReducer extends Reducer<Text, MapWritable, Text, IntWritable> {
+    private MapWritable incrementingMap = new MapWritable();
+
+    protected void reduce(Text key, Iterable<MapWritable> values, Context context) throws IOException, InterruptedException {
+    	Text word = new Text();
+        incrementingMap.clear();
+        
+        for (MapWritable mapWritable : values) {
+        	Set<Writable> neighbors = mapWritable.keySet();
+        	
+            for (Writable neighbor : neighbors) {  
+                IntWritable fromCount = (IntWritable) mapWritable.get(neighbor);
+                if (incrementingMap.containsKey(neighbor)) {
+                    IntWritable count = (IntWritable) incrementingMap.get(neighbor);
+                    count.set(count.get() + fromCount.get());
+                } else {
+                    incrementingMap.put(neighbor, fromCount);
+                	word.set(key + "," + neighbor);
+                    context.write(word, fromCount);
+                }
+            }
+        }
+        
+    }
+}
+
+
+
+/*
 	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 		private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values,
-				Context context
-				) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
 			for (IntWritable val : values) {
 				sum += val.get();
@@ -94,8 +139,16 @@ public class WordCount {
 			context.write(key, result);
 		}
 	}
+*/
+
+
+
+
+
 
 	public static void main(String[] args) throws Exception {
+		WordCount.initLogFile();
+
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "word count");
 
@@ -105,13 +158,33 @@ public class WordCount {
 		job.getConfiguration().set(STOP_WORDS_FILE, "stop_words.txt");
 		
 		job.setJarByClass(WordCount.class);
+		
 		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(MapWritable.class);
+		
+		//job.setCombinerClass(StripesReducer.class);
+		job.setReducerClass(StripesReducer.class);
+
+		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
+
+	
+	  public static void initLogFile() {
+	  		 try {
+	  			 logFile = new FileHandler("logger.log", false);
+	  		 	} catch (SecurityException | IOException e) {
+	  			 e.printStackTrace();
+	  		 }
+	  		 logger = Logger.getLogger("");
+	  		logFile.setFormatter(new SimpleFormatter());
+	  		 logger.addHandler(logFile);
+	  		 logger.setLevel(Level.FINER);
+	  		 }
+	
 }
